e5911cc1e1b0d5fc79b9c36f62e8ee39302e2764
Jeremy C. Russell <jeremycrussell@gmail.com>
2019-01-08 14:49:01 -0600

    adding install for rhel

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,30 @@
 class webhook::install {
   assert_private()
 
-  package { 'webhook':
-    ensure => installed,
+  case $::osfamily {
+    'Debian': {
+      package { 'webhook':
+        ensure => installed,
+      }
+    }
+    'RedHat': {
+      file { '/root/rpms':
+        ensure => 'directory',
+        owner => 'root',
+        group => 'root'
+      } ->
+      file { '/root/rpms/webhook-2.5.0-3.x86_64.rpm':
+        ensure => 'file',
+        owner => 'root',
+        group => 'root',
+        source => 'puppet:///modules/webhook/rpms/webhook-2.5.0-3.x86_64.rpm'
+      } ->
+      package { 'webhook-2.5.0-3.x86_64.rpm':
+        ensure => 'installed',
+        provider => 'rpm',
+        source => '/root/rpms/webhook-2.5.0-3.x86_64.rpm',
+        require => File["/root/rpms/webhook-2.5.0-3.x86_64.rpm"],
+      }
+    }
   }
 }
