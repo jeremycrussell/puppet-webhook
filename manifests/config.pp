ff1caaad097d3f0ff1c1125576096f0c18c23592
Jeremy Russell <jeremycrussell@gmail.com>
2020-05-28 23:58:26 -0500

    fixing multiple hooks

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,23 +2,12 @@
 class webhook::config {
   assert_private()
 
-  ::concat { '/etc/webhook.conf':
+   ::concat { '/etc/webhook.conf':
     ensure         => present,
     owner          => 'root',
     group          => 'root',
     mode           => '0640',
+    format         => 'json-array-pretty',
     ensure_newline => true,
-  }
-
-  ::concat::fragment { 'webhook json-array-opening':
-    target  => '/etc/webhook.conf',
-    order   => '01',
-    content => '[',
-  }
-
-  ::concat::fragment { 'webhook json-array-closing':
-    target  => '/etc/webhook.conf',
-    order   => '99',
-    content => ']',
   }
 }
